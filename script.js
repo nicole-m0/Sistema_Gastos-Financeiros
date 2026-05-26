67055fd7b14d751b08a4b3c068ce1824a309b144
nicole926 <nicolemendes.dev@gmail.com>
2026-05-26 15:57:21 -0300

    commit

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
+// adicionar os valores em receita/despesa/total
+// se  adicionado despesa: diminui em despesa e saldo
+// se adicionado receita: soma em receita e saldo
+
+
+const valorReceita = document.querySelector("#valorReceita");
+    const valorDespesa = document.querySelector("#valorDespesa");
+    const valorSaldo = document.querySelector("#valorSaldo");
+    let contadorReceita = 0;
+    let contadorDespesa = 0;
+    let contadorsaldo = 0;
+
+// Adicionar receita/despesa
 const addBtn = document.querySelector("#btnAdd");
 
 addBtn.addEventListener("click", (event) => {
     event.preventDefault();
-
+    // lista e histórico
     var lista = document.querySelector("#historicoLista");
     var li = document.createElement("li");
-
+    // inputs - DOM
     var descricao = document.querySelector("#descricao");
     var valor = document.querySelector("#valor");
     var categoria = document.querySelector("#categoria");
-
+    // valor digitado
     var descricaoInput = descricao.value;
     var valorInput = valor.value;
     var categoriaInput = categoria.value;
-
+    // concatenação e criação dentro do histórico
     const descricaoTexto = document.createElement("span");
     descricaoTexto.textContent = descricaoInput + " - ";
 
@@ -22,12 +35,24 @@ addBtn.addEventListener("click", (event) => {
     if(categoriaInput === "receita"){
         valorTexto.textContent = `+ R$ ${valorInput}`;
         valorTexto.style.color = "#37f4b5";
+
+        contadorReceita += valorInput;
+        contadorsaldo += valorInput;
     }
+
+    valorReceita.textContent = contadorReceita;
+    valorSaldo.textContent = contadorsaldo;
 
     if(categoriaInput === "despesa"){
         valorTexto.textContent = `- R$ ${valorInput}`;
         valorTexto.style.color = "#de062d";
+
+        contadorDespesa += valorInput;
+        contadorsaldo -= valorInput;
     }
+
+    valorDespesa.textContent = contadorDespesa;
+    valorSaldo.textContent = contadorsaldo
 
     li.appendChild(descricaoTexto);
     li.appendChild(valorTexto);
@@ -37,6 +62,9 @@ addBtn.addEventListener("click", (event) => {
     descricao.value = "";
     valor.value = "";
     categoria.value = "";
+
+
 });
 
-// adicionar os valores em receita/despesa/total
+// contadorP será o <p>, e contadorValor será = 0 ... caso receita/despesa seja adicionada, pega o valorInput += contador, e depois transforma para texto ao <p> (appendChild)
+// para receita/saldo(+), despesa/saldo(-)
