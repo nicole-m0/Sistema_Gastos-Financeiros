57b0781334a59cd0500449e2bdc781b4705b1fbb
nicole926 <nicolemendes.dev@gmail.com>
2026-05-28 15:27:22 -0300

    commit

--- script.js
+++ script.js
@@ -119,3 +119,11 @@ function carregarHistorico() {
 }
 
 carregarHistorico();
+
+// menu
+const menuIcon = document.getElementById("menuIcon");
+const menu = document.getElementById("menu");
+
+menuIcon.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
